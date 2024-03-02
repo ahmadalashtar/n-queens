7be26af2479814faecd8bf1e999e4b27cec9703c
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-02 20:24:17 +0300

    check diagonal violations in a chromosome

--- evaluate.py
+++ evaluate.py
@@ -6,3 +6,11 @@ def checkAdjacentCells(queens):
                 violations += 1
 
     return violations
+
+def checkDiagonally(queens):
+    violations = 0
+    for i in range(len(queens)):
+        for j in range(i+1,len(queens)):
+            if queens[i]==queens[j]+j-i or queens[j]==queens[i]+j-i:
+                violations += 1
+    return violations
