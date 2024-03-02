c54f3ea3d200e9fc6be64d70703d38283f5b4356
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-02 05:04:58 +0300

    initialize population in GA

--- NQueens.py
+++ NQueens.py
@@ -1,5 +1,10 @@
-class NQueens():
-    def solveGA():
-        return
-    def solveMinConflicts():
+from initializePopulation import initializePopulation
+
+class NQueens:
+    def __init__(self,size):
+        self.size = size
+
+    def solveGA(self,populationSize):
+        population = initializePopulation(populationSize,self.size)
+    def solveMinConflicts(self):
         return
