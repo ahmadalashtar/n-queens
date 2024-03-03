0d92ef719cb94c211ff723b69c2a170f195296c1
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-03 15:08:37 +0300

    implement no solution found

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from NQueens import NQueens
 
-queens = 4
-populationSize = 100
-maxTries = 100
+def main():
+    queens = 10
+    populationSize = 100
+    maxTries = 100
 
-nqueens = NQueens(queens)
+    if populationSize%2 != 0:
+        print("Population size must be an even number!")
+        return
+    
+    nqueens = NQueens(queens)
 
-nqueens.solve(populationSize, maxTries)
+    nqueens.solve(populationSize, maxTries)
 
-print(nqueens.solution.queens)
+    if  nqueens.solution is None:
+        print("No solution found!")
+        return
+    
+    print(nqueens.solution.queens)
+        
+
+if __name__ == "__main__":
+    main()
