9494430caeb8238b66e1a999553c6456f82fdea6
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-03 17:04:13 +0300

    remove unnecessary lines

--- variation.py
+++ variation.py
@@ -1,10 +1,7 @@
 from crossover import crossover
 from mutate import mutate
-import random
 
 def variation(pool):
     offsprings = crossover(pool)
     offsprings = mutate(offsprings)
     return offsprings
-
-
