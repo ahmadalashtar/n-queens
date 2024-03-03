faa7e681a17a7a9705ac1bfd6bb7a45fbc319374
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-03 18:01:14 +0300

    remove checkAdjacentCells from evaluate

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,3 @@
-def checkAdjacentCells(queens):
-    violations = 0
-    for i in range(len(queens)):
-        for j in range(i+1,len(queens)):
-            if queens[i]==queens[j]:
-                violations += 1
-
-    return violations
-
 def checkDiagonally(queens):
     violations = 0
     for i in range(len(queens)):
@@ -18,6 +9,5 @@ def checkDiagonally(queens):
 def evaluate(population):
     for chromosome in population:
         fitness = 0
-        fitness += checkAdjacentCells(chromosome.queens)
         fitness += checkDiagonally(chromosome.queens)
         chromosome.fitness = fitness
