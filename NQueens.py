6b061a1a1efa438c0269cd3b2835b27b77b10c93
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-03 17:11:18 +0300

    remove unnecessary lines

--- NQueens.py
+++ NQueens.py
@@ -3,6 +3,7 @@ from evaluate import evaluate
 from selection import selection
 from variation import variation
 from survive import survive
+
 class NQueens:
     def __init__(self,size):
         self.size = size
@@ -16,7 +17,6 @@ class NQueens:
         
         population = initializePopulation(populationSize,self.size)
         for i in range(maxTries):
-            # evaluate population
             evaluate(population)
             for chromosome in population:
                 if chromosome.fitness == 0:
