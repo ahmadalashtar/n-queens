a53f68a1bb37b3776d8f24b6d553f1316bbcf212
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-03 14:24:57 +0300

    shuffle mating pool in crossover.py

---
+++ crossover.py
@@ -0,0 +1,4 @@
+import random
+
+def crossover(pool):
+    random.shuffle(pool)
--- variation.py
+++ variation.py
@@ -3,7 +3,6 @@ from mutate import mutate
 import random
 
 def variation(pool):
-    random.shuffle(pool)
     offsprings = crossover(pool)
     population = mutate(offsprings)
     return population
