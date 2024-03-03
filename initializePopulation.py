cd99016a4cd4fa919016d3c2a2c466e15e2a6e9c
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-03 17:12:36 +0300

    update initialize population to include unique genes

--- initializePopulation.py
+++ initializePopulation.py
@@ -1,13 +1,17 @@
 from Chromosome import Chromosome
 import random
+import copy
 
 def initializePopulation(populationSize,chromosomeSize):
     population = []
+    allowedGenes = []
+    for i in range(chromosomeSize):
+        allowedGenes.append(i+1)
+
     for i in range(populationSize):
         chromosome = Chromosome()
-        for i in range(chromosomeSize):
-            gene = random.randint(1,chromosomeSize)
-            chromosome.queens.append(gene)
+        random.shuffle(allowedGenes)
+        chromosome.queens = copy.deepcopy(allowedGenes)
         population.append(chromosome)
     return population
     
