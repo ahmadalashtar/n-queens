c3c1bfca2b6e77ecd384d933dbf8b883c975bd48
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-03 14:45:55 +0300

    implement crossover

--- crossover.py
+++ crossover.py
@@ -1,4 +1,28 @@
+from Chromosome import Chromosome
 import random
 
 def crossover(pool):
+    offsprings = []
+
     random.shuffle(pool)
+    chromosome = pool[0]
+    crossoverSite = random.randint(1,len(chromosome.queens)-1)
+    for i in range(len(pool)/2):
+        father = pool[i]
+        mother = pool[i+1]
+        firstChild = Chromosome()
+        secondChild = Chromosome()
+
+        for j in range(len(chromosome.queens)):
+            if j < crossoverSite:
+                firstChild.queens.append(father.queens[j])
+                secondChild.queens.append(mother.queens[j])
+
+            else:
+                firstChild.queens.append(mother.queens[j])
+                secondChild.queens.append(father.queens[j])
+    
+        offsprings.append(firstChild)
+        offsprings.append(secondChild)
+    
+    return offsprings
