7221f034bff50e273378f25ec824785b7880d544
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-03 13:58:18 +0300

    implement the selection function selection type is tournament selection k = fifth population unless less than 6 randomly select k chromosomes to fight winner is added to the mating pool

---
+++ selection.py
@@ -0,0 +1,32 @@
+import math
+import random
+
+def getFitness(chromosome):
+    return chromosome.fitness
+
+def selection(population):
+    pool = []
+
+    if len(population)<6:
+        k = 2
+    else:
+        k = math.ceil(len(population)/5)
+    
+    for i in range(len(population)):
+        cage = []
+
+        for i in range(k):
+            index = random.randint(0,len(population))
+            chromosome = population[index]
+            cage.append(chromosome)
+        
+        winner = cage.sort(key=getFitness)[0]
+
+        pool.append(winner)
+    
+    return pool
+        
+
+
+
+
