8bcd586881b8bf67b154870b10fd2f899fa502fa
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-03-03 17:10:39 +0300

    mutate swaps between two random indices

--- mutate.py
+++ mutate.py
@@ -1,12 +1,13 @@
 import random
 
 def mutate(offsprings):
-    probability = 0.1
+    probability = 0.15
 
     for i in range(len(offsprings)):
         for j in range(len(offsprings[0].queens)):
-            mutationProp = random.random()
-            if mutationProp<= probability:
-                offsprings[i].queens[j] = random.randint(1,len(offsprings[0].queens))
+            mutationProbability = random.random()
+            if mutationProbability <= probability:
+                randomIndex = random.randint(0,len(offsprings[0].queens)-1)
+                offsprings[i].queens[j], offsprings[i].queens[randomIndex]  = offsprings[i].queens[randomIndex],  offsprings[i].queens[j]
     
     return offsprings
